e53e9a28e8a48592264a99fef303f5d73ccd28a3
treknuts <trevor.a.knutson@ndsu.edu>
2020-11-29 14:58:00 -0600

    Cosine similarity done. WIP make function to take text and do NLP

--- preprocessor.js
+++ preprocessor.js
@@ -1,3 +1,7 @@
+// Words to ignore (a, an, of, or, and, the)
+
+const stopWords = ["a", "an", "of", "or", "and", "the"];
+
 const getVocabulary = (documents) => {
   var unique = [];
   documents.forEach((doc) => {
@@ -5,7 +9,7 @@ const getVocabulary = (documents) => {
       .toLowerCase()
       .split(" ")
       .forEach((w) => {
-        if (!unique.includes(w)) unique.push(w);
+        if (!unique.includes(w) && !stopWords.includes(w)) unique.push(w);
       });
   });
   return unique;
@@ -25,10 +29,12 @@ const calculateWeight = () => {
 };
 
 function wordCountMap(str) {
-  let words = str.split(" ");
+  let words = str.toLowerCase().split(" ");
   let wordCount = {};
   words.forEach((w) => {
-    wordCount[w] = (wordCount[w] || 0) + 1;
+    if (!stopWords.includes(w)) {
+      wordCount[w] = (wordCount[w] || 0) + 1;
+    }
   });
   return wordCount;
 }
@@ -77,25 +83,42 @@ const calculateIdf = (documents, vocabulary) => {
   return idf;
 };
 
+const calculateTfIdf = (tf, idf) => {
+  tfidf = [];
+  tf.forEach((doc) => {
+    vec = {};
+    for (const word in doc) {
+      vec[word] = doc[word] === 1 ? 1 : doc[word] * idf[word];
+    }
+    tfidf.push(vec);
+  });
+  return tfidf;
+};
+
 const cosineSimilarity = (a, b) => {
   return dotProduct(a, b) / (magnitude(a) * magnitude(b));
 };
 
 function dotProduct(vecA, vecB) {
-  let product = 0;
-  for (let i = 0; i < vecA.length; i++) {
-    product += vecA[i] * vecB[i];
+  var product = 0;
+  for (const word in vecA) {
+    if (Object.keys(vecB).includes(word)) product += vecA[word] * vecB[word];
   }
   return product;
 }
 
 function magnitude(vec) {
   let sum = 0;
-  for (let i = 0; i < vec.length; i++) {
-    sum += vec[i] * vec[i];
+  // for (let i = 0; i < vec.length; i++) {
+  //   sum += vec[i] * vec[i];
+  // }
+  for (const word in vec) {
+    sum += vec[word] * vec[word];
   }
   return Math.sqrt(sum);
 }
+
+const similaritiesFromText = (query, documents) => {};
 
 var query = "How much wood does the wood chuck chuck";
 
@@ -106,13 +129,56 @@ var document2 = "The quick brown fox does some shit";
 
 var document3 = "A wood chipper chips wood";
 
-var documents = [document1, document2, document3, query];
+var documents = [document1, document2, document3];
+
 var vocabulary = getVocabulary(documents);
+
+var freqMap = [];
+documents.forEach((doc) => {
+  freqMap.push(wordCountMap(doc));
+});
+
+// console.log("Document term frequencies: ", freqMap);
 
 idf = calculateIdf(documents, vocabulary);
 
-documents.forEach((doc) => {
-  console.log(wordCountMap(doc));
+var queryMap = wordCountMap(query);
+var queryVec = {};
+for (const word in queryMap) {
+  queryVec[word] = queryMap[word] === 1 ? 1 : queryMap[word] * idf[word];
+}
+
+tfidf = calculateTfIdf(freqMap, idf);
+
+var docMagnitudes = [];
+tfidf.forEach((vec) => {
+  docMagnitudes.push(magnitude(vec));
 });
 
-console.log("IDF Matrix: ", idf);
+var dotProducts = [];
+tfidf.forEach((vec) => {
+  dotProducts.push(dotProduct(queryVec, vec));
+});
+
+similarities = [];
+tfidf.forEach((vec) => {
+  similarities.push(Math.round(cosineSimilarity(queryVec, vec) * 100));
+});
+
+console.log("Query vector: ", queryVec);
+console.log("Document vectors: ", freqMap);
+console.log("TFIDF Matrix: ", calculateTfIdf(freqMap, idf));
+console.log("Dot products: ", dotProducts);
+console.log("Query magnitude: ", magnitude(queryVec));
+console.log("Document magnitudes", docMagnitudes);
+console.log("Similarities", similarities);
+
+// const comparator = require("string-similarity");
+
+// var matches = comparator.findBestMatch(query, documents);
+
+// console.log("Search matches: ", matches);
+
+// matches.ratings.sort((a, b) => (a.rating > b.rating ? -1 : 1));
+
+// console.log("Sorted matches: ", matches);
